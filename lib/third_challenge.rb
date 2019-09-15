4de8bb41d90a795d6109218a1cce28fe6d10bff4
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 10:58:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,8 +26,6 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
           ]
    }
   }
