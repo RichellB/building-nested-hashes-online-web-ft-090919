e21ba594bda604fa708b9dcbb5c702613eb66e1f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 10:56:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,6 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
       :patriarch => {},
       :matriach => {},
       :hero => {},
@@ -9,7 +8,6 @@ def second_challenge
       
       }, 
     :capulet => {
-      #your key/value pairs here!
       :patriarch => {},
       :matriach => {},
       :heroine => {},
