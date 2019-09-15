4ca75be4d55850a90efbcca110edb6140da42aba
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 10:57:24 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,6 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
-      ]
    }
 }
 
